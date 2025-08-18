7a2e30fadb88ee5e6c2c21a764ab2415547bca86
Facundo Martin <106789613+FacundoM22@users.noreply.github.com>
2025-08-17 22:54:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Por ejemplo, smooth scroll al hacer click en enlaces
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 

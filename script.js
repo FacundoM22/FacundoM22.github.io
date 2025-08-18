a33abb4588ea069d28844980286c4c17bbb5007a
Facundo Martin <106789613+FacundoM22@users.noreply.github.com>
2025-08-18 11:03:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// Navbar efecto scroll
-window.addEventListener("scroll", function() {
-  const nav = document.querySelector("nav");
-  nav.classList.toggle("scrolled", window.scrollY > 50);
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    640: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 }
+  }
 });
